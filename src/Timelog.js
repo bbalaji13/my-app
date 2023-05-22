e57d3e0eedaeb675752fbaf1a0f81fd4197dd3d9
balaji <bbalaji13@gmail.com>
2023-05-22 21:44:32 +0530

    sucessful timer 1

--- src/Timelog.js
+++ src/Timelog.js
@@ -7,17 +7,11 @@ function Timelog() {
   const [projectName, setProjectName] = useState('');
   const [date, setDate] = useState(null);
   const [description, setDescription] = useState('');
-  const [summary, setSummary] = useState('');
   const [timelogEntries, setTimelogEntries] = useState([]);
   const [timerRunning, setTimerRunning] = useState(false);
   const [startTime, setStartTime] = useState(null);
   const [stopTime, setStopTime] = useState(null);
-  const [manualStartTime, setManualStartTime] = useState('');
-  const [manualStopTime, setManualStopTime] = useState('');
-  const [sortField, setSortField] = useState('date');
-  const [sortDirection, setSortDirection] = useState('asc');
-  const [fromDate, setFromDate] = useState(null);
-  const [toDate, setToDate] = useState(null);
+  const [entryMode, setEntryMode] = useState('manual');
 
   useEffect(() => {
     if (timerRunning) {
@@ -43,16 +37,10 @@ function Timelog() {
     setDescription(event.target.value);
   };
 
-  const handleSummaryChange = (event) => {
-    setSummary(event.target.value);
-  };
-
   const handleStartTimer = () => {
     setTimerRunning(true);
     setStartTime(new Date());
     setStopTime(null);
-    setManualStartTime('');
-    setManualStopTime('');
   };
 
   const handleStopTimer = () => {
@@ -63,7 +51,7 @@ function Timelog() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (timerRunning) {
+    if (entryMode === 'timer' && timerRunning) {
       handleStopTimer();
     } else {
       addEntry();
@@ -71,15 +59,14 @@ function Timelog() {
   };
 
   const addEntry = () => {
-    const start = timerRunning ? startTime : parseTime(manualStartTime);
-    const stop = timerRunning ? stopTime : parseTime(manualStopTime);
+    const start = entryMode === 'timer' ? startTime : parseTime(startTime);
+    const stop = entryMode === 'timer' ? stopTime : parseTime(stopTime);
     const duration = calculateDuration(start, stop);
 
     const entry = {
       projectName,
       date: date ? date.toLocaleDateString() : new Date().toLocaleDateString(),
       description,
-      summary,
       startTime: formatTime(start),
       stopTime: formatTime(stop),
       duration,
@@ -89,11 +76,8 @@ function Timelog() {
     setProjectName('');
     setDate(null);
     setDescription('');
-    setSummary('');
     setStartTime(null);
     setStopTime(null);
-    setManualStartTime('');
-    setManualStopTime('');
   };
 
   const calculateDuration = (start, stop) => {
@@ -116,168 +100,137 @@ function Timelog() {
   };
 
   const parseTime = (timeString) => {
+    if (!timeString) {
+      return null;
+    }
+
     const [hours, minutes] = timeString.split(':');
-    const currentDate = new Date();
-    return new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      currentDate.getDate(),
-      hours,
-      minutes
-    );
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
   };
 
-  const handleSort = (field) => {
-    if (sortField === field) {
-      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
-    } else {
-      setSortField(field);
-      setSortDirection('asc');
-    }
+  const handleEntryModeChange = (event) => {
+    setEntryMode(event.target.value);
   };
 
-  const filterEntries = () => {
-    let filteredEntries = [...timelogEntries];
-
-    if (fromDate) {
-      filteredEntries = filteredEntries.filter(
-        (entry) => new Date(entry.date) >= new Date(fromDate.setHours(0, 0, 0, 0))
-      );
-    }
-
-    if (toDate) {
-      filteredEntries = filteredEntries.filter(
-        (entry) => new Date(entry.date) <= new Date(toDate.setHours(23, 59, 59, 999))
-      );
-    }
-
-    return filteredEntries;
-  };
-
-  const parseDuration = (duration) => {
-    const [hours, minutes] = duration.split('h ');
-    return parseInt(hours) * 60 + parseInt(minutes);
-  };
-
-  const renderTimer = () => {
-    if (timerRunning) {
-      return (
-        <div className="timer-container">
-          <span>{formatTime(startTime)}</span>
-        </div>
-      );
-    }
-
-    return null;
-  };
+  const sortedEntries = timelogEntries.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   return (
     <div className="timelog-container">
       <form onSubmit={handleSubmit}>
-        <div className="form-row">
-          <label>Project Name:</label>
-          <input type="text" value={projectName} onChange={handleProjectNameChange} required />
+        <div className="entry-mode-container">
+          <label>
+            <input
+              type="radio"
+              value="manual"
+              checked={entryMode === 'manual'}
+              onChange={handleEntryModeChange}
+            />
+            Manual Entry
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="timer"
+              checked={entryMode === 'timer'}
+              onChange={handleEntryModeChange}
+            />
+            Timer
+          </label>
         </div>
-        {timerRunning ? (
-          renderTimer()
-        ) : (
-          <div className="form-row">
-            <div className="form-column">
+        <div className="form-row">
+          <div className="form-field">
+            <label>Project Name:</label>
+            <input
+              type="text"
+              value={projectName}
+              onChange={handleProjectNameChange}
+              required
+            />
+          </div>
+          {entryMode === 'manual' && (
+            <div className="form-field">
               <label>Date:</label>
-              <DatePicker selected={date} onChange={handleDateChange} dateFormat="dd/MM/yyyy" />
-            </div>
-            <div className="form-column">
-              <label>Manual Start Time:</label>
-              <input
-                type="time"
-                value={manualStartTime}
-                onChange={(e) => setManualStartTime(e.target.value)}
-                required={!timerRunning}
+              <DatePicker
+                selected={date}
+                onChange={handleDateChange}
+                dateFormat="dd/MM/yyyy"
               />
             </div>
-            <div className="form-column">
-              <label>Manual Stop Time:</label>
+          )}
+          <div className="form-field">
+            <label>Description:</label>
+            <textarea
+              value={description}
+              onChange={handleDescriptionChange}
+              required
+            ></textarea>
+          </div>
+        </div>
+        {entryMode === 'timer' && !timerRunning && (
+          <div className="form-row">
+            <button className="start-timer-btn" onClick={handleStartTimer}>
+              Start Timer
+            </button>
+          </div>
+        )}
+        {entryMode === 'timer' && timerRunning && (
+          <div className="form-row">
+            <button className="stop-timer-btn" onClick={handleStopTimer}>
+              Stop Timer
+            </button>
+          </div>
+        )}
+        {entryMode === 'manual' && (
+          <div className="form-row">
+            <div className="form-field">
+              <label>Start Time:</label>
               <input
                 type="time"
-                value={manualStopTime}
-                onChange={(e) => setManualStopTime(e.target.value)}
-                required={!timerRunning}
+                value={startTime || ''}
+                onChange={(e) => setStartTime(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label>Stop Time:</label>
+              <input
+                type="time"
+                value={stopTime || ''}
+                onChange={(e) => setStopTime(e.target.value)}
+                required
               />
             </div>
           </div>
         )}
         <div className="form-row">
-          {timerRunning ? (
-            <button type="button" onClick={handleStopTimer} disabled={!timerRunning}>
-              Stop Timer
-            </button>
-          ) : (
-            <button type="button" onClick={handleStartTimer}>
-              Start Timer
-            </button>
-          )}
-          <button type="submit">{timerRunning ? 'Stop Timer & Add Entry' : 'Add Entry'}</button>
-        </div>
-        <div className="form-row">
-          <label>Description:</label>
-          <textarea
-            value={description}
-            onChange={handleDescriptionChange}
-            placeholder="Enter description"
-            required
-          />
-        </div>
-        <div className="form-row">
-          <label>Summary:</label>
-          <textarea value={summary} onChange={handleSummaryChange} placeholder="Enter summary" required />
+          <button type="submit" className="submit-btn">
+            {entryMode === 'timer' && timerRunning ? 'Stop Timer & Add Entry' : 'Add Entry'}
+          </button>
         </div>
       </form>
-      <div className="filter-row">
-        <label>From Date:</label>
-        <DatePicker selected={fromDate} onChange={setFromDate} dateFormat="dd/MM/yyyy" />
-        <label>To Date:</label>
-        <DatePicker selected={toDate} onChange={setToDate} dateFormat="dd/MM/yyyy" />
-      </div>
-      <table>
+      <table className="timelog-table">
         <thead>
           <tr>
-            <th onClick={() => handleSort('projectName')}>
-              Project Name {sortField === 'projectName' && sortDirection === 'asc' ? '▲' : '▼'}
-            </th>
-            <th onClick={() => handleSort('date')}>
-              Date {sortField === 'date' && sortDirection === 'asc' ? '▲' : '▼'}
-            </th>
-            <th onClick={() => handleSort('startTime')}>
-              Start Time {sortField === 'startTime' && sortDirection === 'asc' ? '▲' : '▼'}
-            </th>
-            <th onClick={() => handleSort('stopTime')}>
-              Stop Time {sortField === 'stopTime' && sortDirection === 'asc' ? '▲' : '▼'}
-            </th>
-            <th onClick={() => handleSort('duration')}>
-              Duration {sortField === 'duration' && sortDirection === 'asc' ? '▲' : '▼'}
-            </th>
+            <th>Date</th>
+            <th>Start Time</th>
+            <th>Stop Time</th>
             <th>Description</th>
-            <th>Summary</th>
+            <th>Duration</th>
+            <th>Project Name</th>
           </tr>
         </thead>
         <tbody>
-          {filterEntries()
-            .sort((a, b) => {
-              const aValue = a[sortField];
-              const bValue = b[sortField];
-              return sortDirection === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
-            })
-            .map((entry, index) => (
-              <tr key={index}>
-                <td>{entry.projectName}</td>
-                <td>{entry.date}</td>
-                <td>{entry.startTime}</td>
-                <td>{entry.stopTime}</td>
-                <td>{entry.duration}</td>
-                <td>{entry.description}</td>
-                <td>{entry.summary}</td>
-              </tr>
-            ))}
+          {sortedEntries.map((entry, index) => (
+            <tr key={index}>
+              <td>{entry.date}</td>
+              <td>{entry.startTime}</td>
+              <td>{entry.stopTime}</td>
+              <td>{entry.description}</td>
+              <td>{entry.duration}</td>
+              <td>{entry.projectName}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
